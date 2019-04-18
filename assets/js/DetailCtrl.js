6cae88322e631bad84c405ea70887a119e5249f4
enesyteam <enesyteam@gmail.com>
2019-04-18 14:37:37 +0700

    update

--- assets/js/DetailCtrl.js
+++ assets/js/DetailCtrl.js
@@ -9,43 +9,43 @@ goSua.controller('DetailCtrl', function( $rootScope, $scope, $http, $window, $do
 	switch($stateParams.content) {
         case "tv1":
             $scope.detailPage = "/pages/detail-tv1.html";
-            $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net'
             break;
         case "tv2":
             $scope.detailPage = "/pages/detail-tv2.html";
-            $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net'
             break;
         case "tv3":
             $scope.detailPage = "/pages/detail-tv3.html"
-            $scope.redirectUrl = 'http://amulet3.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet3.biquyetlamgiau.net'
             break;
         case "tv4":
             $scope.detailPage = "/pages/detail-tv4.html"
-            $scope.redirectUrl = 'http://amulet4.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet4.biquyetlamgiau.net'
             break;
         case "tv5":
             $scope.detailPage = "/pages/detail-tv5.html"
-            $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net'
             break;
         case "tv6":
             $scope.detailPage = "/pages/detail-tv6.html"
-            $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net'
+            // $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net'
             break;
         case "st1":
             $scope.detailPage = "/pages/detail-story1.html"
-            $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net';
+            // $scope.redirectUrl = 'http://amulet1.biquyetlamgiau.net';
             break;
         case "st2":
             $scope.detailPage = "/pages/detail-story2.html"
-            $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net';
+            // $scope.redirectUrl = 'http://amulet2.biquyetlamgiau.net';
             break;
         case "st3":
             $scope.detailPage = "/pages/detail-story3.html"
-            $scope.redirectUrl = 'http://amulet3.biquyetlamgiau.net';
+            // $scope.redirectUrl = 'http://amulet3.biquyetlamgiau.net';
             break;
         case "st4":
             $scope.detailPage = "/pages/detail-story4.html"
-            $scope.redirectUrl = 'http://amulet4.biquyetlamgiau.net';
+            // $scope.redirectUrl = 'http://amulet4.biquyetlamgiau.net';
             break;
         default:
     }
